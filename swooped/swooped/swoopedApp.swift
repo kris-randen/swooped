e33df893fe24faf49050346d302add3861c8daaa
kris-randen <kris.randen@gmail.com>
2024-05-03 19:01:53 +0530

    Initiated the swooped app on Xcode.

---
+++ swooped/swooped/swoopedApp.swift
@@ -0,0 +1,17 @@
+//
+//  swoopedApp.swift
+//  swooped
+//
+//  Created by Krishnaswami Rajendren on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct swoopedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
